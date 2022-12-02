96d0381790b41e9c53938789d479b470fefbdecf
jibananando <jibananando.pramanik@gmail.com>
2022-12-02 22:55:57 +0600

    delte user

--- index.js
+++ index.js
@@ -92,8 +92,14 @@ async function run() {
             const options = await userCollection.find(query).toArray();
             res.send(options);
         })
+        app.delete('/deleteSellerAndBuyer/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const options = await userCollection.deleteOne(query);
+            res.send(options);
+        })
 
-        // deleteSellerAndBuyer
+
     }
     finally {
 
